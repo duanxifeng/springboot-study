af66c6ed76ba2db40db2a1fdfd6e417d4dc4ea8b
lijianqing <lijianqing.linklogis.com>
2019-04-23 14:13:18 +0800

    消息确认机制

--- rabbitmq/src/main/java/zero/springboot/study/rabbitmq/direct/HelloReceiver.java
+++ rabbitmq/src/main/java/zero/springboot/study/rabbitmq/direct/HelloReceiver.java
@@ -4,14 +4,20 @@
  */
 package zero.springboot.study.rabbitmq.direct;
 
+import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 import zero.springboot.study.rabbitmq.model.User;
 
+import java.io.IOException;
+
 /**
  * 监听hello队列
+ *
  * @author lijianqing
  * @version 1.0
  * @ClassName HelloReceiver
@@ -23,7 +29,16 @@ import zero.springboot.study.rabbitmq.model.User;
 public class HelloReceiver {
 
     @RabbitHandler
-    public void processUser(User user) {
+    public void processUser(User user, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) {
         log.info("收到消息：{}", user);
+        // 手动ACK
+        try {
+//            //消息确认
+            channel.basicAck(tag, false);
+            //否认消息，重新入队列然后一直重新消费
+//            channel.basicNack(tag, false, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
