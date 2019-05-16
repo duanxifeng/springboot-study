b9dd98f25bc1d1276a62d8f279d1444d63714e83
lijianqing <lijianqing.linklogis.com>
2019-05-16 20:25:45 +0800

    异步任务Async 结果返回

--- async/src/main/java/zero/springboot/study/async/service/AsyncTask.java
+++ async/src/main/java/zero/springboot/study/async/service/AsyncTask.java
@@ -6,9 +6,11 @@ package zero.springboot.study.async.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
+import java.util.concurrent.Future;
 
 /**
  * @author lijianqing
@@ -32,11 +34,25 @@ public class AsyncTask {
     }
 
     @Async("taskExecutor")
-    public void pay() throws InterruptedException {
-        log.info("开始做任务3：支付");
+    public Future<String> pay() throws InterruptedException {
+        log.info("开始做异步返回结果任务2：支付");
         long start = System.currentTimeMillis();
         Thread.sleep(random.nextInt(10000));
         long end = System.currentTimeMillis();
         log.info("完成任务2，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("会员服务完成");
+    }
+
+    /**
+     * 返回结果的异步调用
+     * @throws InterruptedException
+     */
+    @Async("taskExecutor")
+    public void vip() throws InterruptedException {
+        log.info("开始做任务5：会员");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        log.info("开始做异步返回结果任务5，耗时：" + (end - start) + "毫秒");
     }
 }
--- async/src/main/java/zero/springboot/study/async/service/OrderService.java
+++ async/src/main/java/zero/springboot/study/async/service/OrderService.java
@@ -6,7 +6,12 @@ package zero.springboot.study.async.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @author lijianqing
@@ -17,6 +22,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class OrderService {
+    public static Random random = new Random();
+
 
     @Autowired
     private AsyncTask asyncTask;
@@ -24,8 +31,18 @@ public class OrderService {
     public void doShop() {
         try {
             createOrder();
-            asyncTask.pay();
-            asyncTask.sendSms();
+            Future<String> pay = asyncTask.pay();
+            if (pay.isDone()) {
+                try {
+                    String result = pay.get();
+                    log.info("异步任务返回结果{}", result);
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+                asyncTask.vip();
+                asyncTask.sendSms();
+            }
+            otherJob();
         } catch (InterruptedException e) {
             log.error("异常", e);
         }
@@ -34,4 +51,21 @@ public class OrderService {
     public void createOrder() {
         log.info("开始做任务1：下单成功");
     }
+
+    /**
+     * 错误使用，不会异步执行：调用方与被调方不能在同一个类。主要是使用了动态代理，同一个类的时候直接调用，不是通过生成的动态代理类调用
+     */
+    @Async("taskExecutor")
+    public void otherJob() {
+        log.info("开始做任务4：物流");
+        long start = System.currentTimeMillis();
+        try {
+            Thread.sleep(random.nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        log.info("完成任务4，耗时：" + (end - start) + "毫秒");
+    }
+
 }
